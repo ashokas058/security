4a325489031ba345d542f24a3cf36c2d88adcd54
Ashok AS <ashokas058>
2021-05-12 18:49:34 -0700

    Final_Modification

--- app/src/main/java/com/example/ashok_ray/security/Module_DATABASE/CLS_DbCoreValues.java
+++ app/src/main/java/com/example/ashok_ray/security/Module_DATABASE/CLS_DbCoreValues.java
@@ -1,17 +1,16 @@
 package com.example.ashok_ray.security.Module_DATABASE;
 
-/**
- * Created by Ashok_Ray on 22-01-2019.
- */
-
-public final class sqldata {
+public class CLS_DbCoreValues {
 
 
-    public static class  sqlstatic{
-public static  final String table_name="users";
-public static final String username="username";
-public static final String url="url_profile";
-public static final String email="email";
 
-    }
+        public static class  sqlstatic{
+            public static  final String table_name="users";
+            public static final String username="username";
+            public static final String url="url_profile";
+            public static final String email="email";
+
+        }
+
 }
+
